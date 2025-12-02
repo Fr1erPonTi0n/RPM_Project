f894c64131eb2b2494b10317ad92698f924ba28e
Romsm1 <semenkovroman06@gmail.com>
2025-12-02 16:12:00 +0900

    Update helper.py

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,7 +1,4 @@
 # ДОПОЛНИТЕЛЬНАЯ ФУНКЦИЯ ДЛЯ ОБЩИХ УТИЛИТ
-
-
-# ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
 def parse_datetime(datetime_str: str) -> datetime:  # Парсинг даты в формате ISO
     try:
         return datetime.fromisoformat(datetime_str.replace('Z', '+00:00'))  # Преобразуем строку в datetime
