71e9fc0bb3d0b5a0e9590eb2e68aa09a617643d0
Fr1erPonTi0n <is02n15ds@bk.ru>
2025-12-14 22:37:02 +0900

    modified:   config.py 	new file:   requirements.txt

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 # КОНФИГУРАЦИОННЫЙ ФАЙЛ
 
 import os
+import sys
 
-DATABASE_URL = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database.db')}"
+def get_database_path():
+    """Определяет путь к базе данных для разных режимов запуска"""
+    if getattr(sys, 'frozen', False):
+        base_dir = os.path.dirname(sys.executable) # Если запущено как EXE
+    else:
+        base_dir = os.path.dirname(os.path.abspath(__file__)) # Если запущено как скрипт Python
+    return os.path.join(base_dir, "database.db")
+
+DATABASE_URL = f"sqlite:///{get_database_path()}"
